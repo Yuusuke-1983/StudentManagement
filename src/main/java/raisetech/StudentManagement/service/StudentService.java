2be90eddc8e2e57d4de21cdae1a7571c7f214bcd
yuusuke-suzuki <suzuki.yuusuke@link-academy.co.jp>
2024-05-07 18:05:34 +0900

    20240507

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -6,7 +6,6 @@ import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.repository.StudentRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,24 +19,10 @@ public class StudentService {
     }
 
     public List<Student> searchStudentList(){
-        // 検索処理
-        List<Student> list = repository.search();
-
-        // 絞り込みをする。年齢が30代の人の見を抽出する。
-        // 抽出したリストをコントローラーに返す
-        List<Student> returnList = new ArrayList<Student>();
-        for (Student student : list) {
-            if (student.getAge() >= 30 && student.getAge() <= 39) {
-                returnList.add(student);
-            }
-        }
-
-        return returnList;
+        return repository.search();
     }
 
     public List<StudentCourse> searchStudentCourseList(){
-        // 絞り込み検索で「Javaコース」のコース情報のみ抽出する。
-        // 抽出したリストをコントローラーに返す。
         return repository.searchStudentCourse();
     }
 }
